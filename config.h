490500ec1714a001342756dea1e3752cdef5e141
Jules Gottraux <jules.gottraux@epfl.ch>
2022-04-26 10:58:30 +0200

    Setup personal fonts

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
-static int borderpx = 2;
+static char *font = "DejaVu Sans Mono:size=12:antialias=true:autohint=true";
+static char *font2[] = { "Font Awesome 6 Free:pixelsize=11:antialias=true:autohint=true" };
+static int borderpx = 4;
 
 /*
  * What program is execed by st depends of these precedence rules:
